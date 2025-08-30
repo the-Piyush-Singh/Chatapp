49a33b749b53d498d8e990674fa26726b19a0c3d
the-Piyush-Singh <2200521520036@ietlucknow.ac.in>
2025-08-30 15:09:08 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ const PORT=process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-     origin:"http://localhost:5173",
+     origin:"https://chatapp-1-juyt.onrender.com",
      credentials:true
 }))
 
