137a9b8c4de1f23c5aeeaf8b0482df1d657c3a4d
the-Piyush-Singh <2200521520036@ietlucknow.ac.in>
2025-08-30 17:06:57 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://chatapp-1-juyt.onrender.com/api", // ensure /api if your routes are prefixed
+  baseURL: "https://chatapp-jm10.onrender.com/api", // ensure /api if your routes are prefixed
   withCredentials: true, // ✅ send cookies automatically
 });
