c1e7dad92aad2744240f86ec676020e0d804f8cb
the-Piyush-Singh <2200521520036@ietlucknow.ac.in>
2025-08-30 15:08:04 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance=axios.create({
-    baseURL:"http://localhost:5001/api",
+    baseURL:"https://chatapp-jm10.onrender.com/api",
     withCredentials:true,
 })
